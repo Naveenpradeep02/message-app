93cea2c5c04bc8797319966a85850f318cb5b70c
naveenpradeep0987@gmail.com <Naveenpradeep02.noreply.github.com>
2025-04-01 14:12:07 +0530

    update

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 export const useThemeStore = create((set) => ({
   theme: localStorage.getItem("chat-theme") || "coffee",
   setTheme: (theme) => {
-    localStorage.setItem("chat-theme", theme);
+    localStorage.setItem("dark", theme);
     set({ theme });
   },
 }));
